292f59de11856c4acaf581b7ce6ba963575b7b5c
Alipori PC <alipori.bot@gmail.com>
2025-01-16 21:16:48 -0600

    Descripción de los cambios

---
+++ src/repository/posts.js
@@ -0,0 +1,13 @@
+const posts = [
+  {
+    title: "Copymax Rebranding",
+    date: "ola",
+    author: "John Doe",
+    description: "This is post 1 description.",
+    categories: ["astro", "react"],
+    href: "post1",
+    image: "/coypmaxwallpaper.png",
+  },
+];
+
+export default posts;
